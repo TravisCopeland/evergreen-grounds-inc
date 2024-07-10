b45e5f90fb8a39eb1fa45618c26e2ac04b1867f3
TravisCopeland <copeland.travis04@gmail.com>
2024-07-10 18:26:55 -0400

    Added placeholder Header, Footer & NavBar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,6 @@
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 import "@/assets/styles/globals.css";
 
 export const metadata = {
@@ -10,7 +13,10 @@ const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
 			<body>
+				<Header />
+				<NavBar />
 				<main>{children}</main>
+				<Footer />
 			</body>
 		</html>
 	);
