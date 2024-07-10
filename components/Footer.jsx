2a6e6c907bb0a11b7e257f1e062e2ff282ee3363
TravisCopeland <copeland.travis04@gmail.com>
2024-07-10 19:39:45 -0400

    Added basic Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+// import logo from "@/public/next.svg";
+
 const Footer = () => {
-	return <div>Footer</div>;
+	const currentYear = new Date().getFullYear();
+
+	return (
+		<footer className="bg-zinc py-4 mt-4">
+			<div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+				<div className="mb-4 md:mb-0">
+					{/* <Image src={logo} alt="Logo" className="h-8 w-auto" /> */}
+				</div>
+				<div>
+					<p className="text-sm text-white mt-2 md:mt-0">
+						&copy; {currentYear} Evergreen Grounds Inc. All rights reserved.
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
