8c69cc7e1f5bcadefa1f68e28dd3882f6c0ea37c
TravisCopeland <copeland.travis04@gmail.com>
2024-07-12 20:51:49 -0400

    Removed margin on Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<footer className="bg-zinc py-4 mt-4">
+		<footer className="bg-zinc py-4">
 			<div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
 				<div className="mb-4 md:mb-0">
 					{/* <Image src={logo} alt="Logo" className="h-8 w-auto" /> */}
