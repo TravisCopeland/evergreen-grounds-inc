16cb1ad09a6467a30369ed193e61f14a1b6c4ae5
TravisCopeland <copeland.travis04@gmail.com>
2024-07-17 19:13:47 -0400

    Added logo to Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-// import logo from "@/public/next.svg";
+import logo from "@/assets/images/logo-standard-white.png";
 
 const Footer = () => {
 	const currentYear = new Date().getFullYear();
@@ -8,11 +8,11 @@ const Footer = () => {
 		<footer className="bg-zinc py-4">
 			<div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
 				<div className="mb-4 md:mb-0">
-					{/* <Image src={logo} alt="Logo" className="h-8 w-auto" /> */}
+					<Image src={logo} alt="Logo" className="h-10 lg:h-18 w-auto" />
 				</div>
 				<div>
 					<p className="text-sm text-white mt-2 md:mt-0">
-						&copy; {currentYear} Evergreen Grounds Inc. All rights reserved.
+						&copy; {currentYear} Evergreen Grounds, Inc. All rights reserved.
 					</p>
 				</div>
 			</div>
