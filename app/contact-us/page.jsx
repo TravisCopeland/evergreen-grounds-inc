a0b8e18ec577391f9b3d126c2b51cca1e39e53b1
TravisCopeland <copeland.travis04@gmail.com>
2024-07-17 20:25:33 -0400

    Added ContactBox

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,5 +1,21 @@
+import ContactBox from "@/components/ContactBox";
+import RequestQuoteForm from "@/components/RequestQuoteForm";
+import { FaPhone, FaEnvelope } from "react-icons/fa";
+
 const ContactUsPage = () => {
-	return <div>ContactUsPage</div>;
+	return (
+		<section className="bg-zinc flex flex-col md:flex-row py-6">
+			<div className="flex flex-col">
+				<ContactBox name="Email Address" content="evergreengrounds73@yahoo.com">
+					<FaEnvelope size={36} />
+				</ContactBox>
+				<ContactBox name="Phone Number" content="(770) 262-2190">
+					<FaPhone size={36} />
+				</ContactBox>
+			</div>
+			<RequestQuoteForm />
+		</section>
+	);
 };
 
 export default ContactUsPage;
---
+++ components/ContactBox.jsx
@@ -0,0 +1,13 @@
+const ContactBox = ({ children, name, content }) => {
+	return (
+		<div className="hidden text-white md:flex justify-center p-16 bg-white bg-opacity-5 border-transparent rounded-lg mt-16 ml-16">
+			<div className="flex flex-col gap-4">
+				<div className="flex justify-center">{children}</div>
+				<h3 className="flex justify-center font-bold text-lime">{name}</h3>
+				<p className="flex justify-center">{content}</p>
+			</div>
+		</div>
+	);
+};
+
+export default ContactBox;
