fae5e2df9bfd085f05f45fb3083812b67cd948ae
TravisCopeland <copeland.travis04@gmail.com>
2024-07-20 16:00:11 -0400

    Added ServiceInfoBox & updated services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,69 @@
+"use client";
+import { useRouter } from "next/navigation";
+import ServicesInfoBox from "@/components/ServicesInfoBox";
+
 const ServicesPage = () => {
-	return <div>ServicesPage</div>;
+	const maintenanceInfo = [
+		"Mowing",
+		"Edging & Blowing",
+		"Leaf & Stick Clean Up",
+		"Seasonal Pruning",
+		"Flower Bed Weed Control",
+		"Shrub Pruning",
+	];
+	const installationInfo = [
+		"Sod",
+		"Plant Design & Install",
+		"Grading",
+		"Mulch",
+		"Pinestraw",
+	];
+	const drainageInfo = ["Irrigation", "Drainage"];
+
+	const router = useRouter();
+
+	return (
+		<section className="bg-zinc p-2 md:p-20">
+			<h1 className="text-white text-xl md:text-3xl font-bold text-center m-4 md:m-8">
+				We are dedicated to working with our clients to achieve their goals
+			</h1>
+			<hr className="border-solid rounded" />
+			<h2 className="text-white text-lg md:text-2xl text-center m-4 md:m-8">
+				<i>
+					<b>Over 4 Decades</b>
+				</i>{" "}
+				of Horticulture Experience
+			</h2>
+			<button
+				type="button"
+				onClick={() => router.push("/contact-us")}
+				className="bg-lime hover:bg-lime-dark text-white p-2 rounded-lg w-full my-4 md:my-0"
+			>
+				Request Free Quote
+			</button>
+			<h3 className="text-white text-xl md:text-3xl font-bold text-center mt-4 md:mt-8">
+				Services Offered
+			</h3>
+			<div className="flex flex-col md:flex-row md:justify-between">
+				<ServicesInfoBox
+					label={"Landscape Maintenance"}
+					infoArray={maintenanceInfo}
+				/>
+				<ServicesInfoBox
+					label={"Landscape Installation"}
+					infoArray={installationInfo}
+				/>
+				<ServicesInfoBox label={"Drainage Systems"} infoArray={drainageInfo} />
+			</div>
+			<button
+				type="button"
+				onClick={() => router.push("/contact-us")}
+				className="bg-lime hover:bg-lime-dark text-white p-2 rounded-lg w-full my-4 md:my-0"
+			>
+				Request Free Quote
+			</button>
+		</section>
+	);
 };
 
 export default ServicesPage;
---
+++ components/ServicesInfoBox.jsx
@@ -0,0 +1,26 @@
+import { FaDotCircle } from "react-icons/fa";
+
+const ServicesInfoBox = ({ label, infoArray }) => {
+	return (
+		<div className="text-white flex md:mx-4 justify-center p-8 bg-white bg-opacity-5 border-transparent rounded-lg my-2 md:my-14 md:w-1/3">
+			<div className="flex flex-col gap-4">
+				<h3 className="flex justify-center text-lg md:text-2xl font-bold text-lime md:mb-2">
+					{label}
+				</h3>
+				<ul className="text-sm md:text-lg">
+					{infoArray &&
+						infoArray.map((info, index) => (
+							<div key={index}>
+								<li className="flex">
+									<FaDotCircle className="p-1 m-1" />
+									{info}
+								</li>
+							</div>
+						))}
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default ServicesInfoBox;
